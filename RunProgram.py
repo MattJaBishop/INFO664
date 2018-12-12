c9faa923b9aec8fc24a325bde833d2af88e763e3
MattJaBishop <35987140+MattJaBishop@users.noreply.github.com>
2018-12-12 17:30:30 -0500

    RunProgram Created
    One Python file to run

---
+++ RunProgram.py
@@ -0,0 +1,20 @@
+
+import AnnualReportPull
+
+import ServicePull
+
+import TestingPull
+
+import cleantestingcsv
+
+print("Process complete! Now edit in Power BI.")
+
+
+#Alternate way to run programs:
+
+# import os 
+# os.system('python AnnualReportPull.py')
+# os.system('python ServicePull.py')
+# os.system('python TestingPull.py')
+# os.system('python cleantestingcsv.py')
+
